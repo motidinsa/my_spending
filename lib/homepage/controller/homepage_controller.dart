025569c0e136a0ea0920629b2bc3076a6d0352ce
Moti <dinsamoti@gmail.com>
2025-02-26 17:02:27 +0300

    added sample bottom bar

---
+++ lib/homepage/controller/homepage_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class HomepageController extends GetxController{
+  int selectedIndex = 0;
+  updateIndex(int index){
+    selectedIndex = index;
+    update();
+  }
+}
